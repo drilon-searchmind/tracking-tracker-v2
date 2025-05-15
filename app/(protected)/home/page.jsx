1c052c9664e3eb9a720a176de1c479b302174e78
drilon-searchmind <dbr@searchmind.dk>
2025-05-15 15:15:07 +0200

    :lipstick: layout and css changes

--- app/(protected)/home/page.jsx
+++ app/(protected)/home/page.jsx
@@ -4,7 +4,7 @@ export default function HomePage() {
             <div className="grid grid-cols-12 gap-10 items-center">
                 <span className="col-span-5">
                     <h4 class="mb-4.5 text-lg font-medium text-black dark:text-white">Searchmind Hub - Home</h4>
-                    <h1 className="mb-5 pr-16 text-3xl font-bold text-black xl:text-[44px]">Welcome to your Searchmind Hub homepage</h1>
+                    <h1 className="mb-5 pr-16 text-3xl font-bold text-black xl:text-[44px] inline-grid z-10">Welcome to your <span className="bg-titlebg relative w-full bottom-0 z-10">Searchmind Hub</span> homepage</h1>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore cupiditate perspiciatis eligendi laboriosam eaque ea, autem est. Minus, ipsum sequi! Modi nulla nihil deleniti mollitia dignissimos quibusdam minus esse necessitatibus.</p>
                     <button className="mt-10 bg-[var(--color-secondary-searchmind)] py-3 px-8 rounded-full text-white hover:cursor">
                         Get Started

19798712494689298646aa146a9183f26602e086
drilon-searchmind <dbr@searchmind.dk>
2025-06-05 11:29:59 +0200

    :construction: wip submenu

--- app/(protected)/dashboard/layout.jsx
+++ app/(protected)/dashboard/layout.jsx
@@ -1,13 +1,14 @@
+import { PiCaretDownThin } from "react-icons/pi";
+
 export default function DashboardLayout({ children }) {
     return (
         <section id="DashboardLayout" className="">
             <nav className="flex justify-between items-center pt-6 pb-3 border-t border-gray-200 mb-5 bg-white">
                 <ul className="flex gap-5">
                     <li><a href="#" className="hover:text-blue-500 text-sm">Overview</a></li>
-                    <li><a href="#" className="hover:text-blue-500 text-sm">Overview Day</a></li>
-                    <li><a href="#" className="hover:text-blue-500 text-sm">Performance Dashboard</a></li>
-                    <li><a href="#" className="hover:text-blue-500 text-sm">SEO Dashboard</a></li>
-                    <li><a href="#" className="hover:text-blue-500 text-sm">Pace Report</a></li>
+                    <li><a href="#" className="hover:text-blue-500 text-sm">Performance Dashboard </a></li>
+                    <li className="flex items-center gap-2"><a href="#" className="hover:text-blue-500 text-sm">Service Dashboard</a><PiCaretDownThin /></li>
+                    <li className="flex items-center gap-2"><a href="#" className="hover:text-blue-500 text-sm">Tools</a><PiCaretDownThin /></li>
                     <li><a href="#" className="hover:text-blue-500 text-sm">Config</a></li>
                 </ul>
                 <span>

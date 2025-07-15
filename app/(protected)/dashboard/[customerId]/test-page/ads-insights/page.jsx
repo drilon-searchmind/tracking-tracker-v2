ac18dd1ca10b4ef557d70419936da7759c3ac4e5
drilon-searchmind <dbr@searchmind.dk>
2025-07-15 15:40:19 +0200

    :construction: bigQuery connection + testing pages

---
+++ app/(protected)/dashboard/[customerId]/test-page/ads-insights/page.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { queryBigQueryAdsInsights } from "@/lib/bigQueryConnect";
+
+export default async function TestPage({ params }) {
+    const customerId = "airbyte_humdakin_dk"
+    const tableId = `performance-dashboard-airbyte`;
+
+    try {
+        const data = await queryBigQueryAdsInsights({
+            tableId,
+            customerId,
+            queryParams: {},
+            limit: 100,
+        });
+
+        return (
+            <div>
+                <h1>Analytics for {customerId} (campaign) </h1>
+                <pre>{JSON.stringify(data, null, 2)}</pre>
+            </div>
+        );
+    } catch (error) {
+        console.error("Error in TestPage:", error);
+        return (
+            <div>
+                <h1>Analytics for {customerId}</h1>
+                <div>Error: {error.message}</div>
+            </div>
+        );
+    }
+}

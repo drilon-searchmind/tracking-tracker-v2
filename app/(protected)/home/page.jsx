abf056985510790056bd99c10c169973c119a570
drilon-searchmind <dbr@searchmind.dk>
2025-08-27 09:53:55 +0200

    :hammer: redirect to /home from root

--- app/(protected)/home/page.jsx
+++ app/(protected)/home/page.jsx
@@ -19,7 +19,7 @@ export default function HomePage() {
                     <h4 class="mb-4.5 text-lg font-light text-zinc-700 dark:text-white">Searchmind Apex - Home</h4>
                     <h1 className="mb-5 pr-16 text-3xl font-bold text-black xl:text-[44px] inline-grid z-10">Welcome to Your <span className="bg-titlebg relative w-full bottom-0 z-10">Searchmind Apex</span> Homepage</h1>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore cupiditate perspiciatis eligendi laboriosam eaque ea, autem est. Minus, ipsum sequi! Modi nulla nihil deleniti mollitia dignissimos quibusdam minus esse necessitatibus.</p>
-                    <button className="mt-10 bg-[var(--color-secondary-searchmind)] py-3 px-8 rounded text-white hover:cursor">
+                    <button className="mt-10 bg-[var(--color-primary-searchmind)] py-3 px-8 rounded text-white hover:cursor">
                         Get Started
                     </button>
                 </span>
--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,27 @@
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-    return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-                <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left">
-                    <h1 className="mb-5 pr-16 text-3xl font-bold text-black xl:text-[44px]">Welcome to <br/> Searchmind Apex</h1>
+    redirect('/home');
 
-                </ol>
-            </main>
-            <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Searchmind 2025
-                </a>
-            </footer>
-        </div>
-    );
+    // return (
+    //     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    //         <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    //             <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left">
+    //                 <h1 className="mb-5 pr-16 text-3xl font-bold text-black xl:text-[44px]">Welcome to <br/> Searchmind Apex</h1>
+
+    //             </ol>
+    //         </main>
+    //         <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+    //             <a
+    //                 className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+    //                 href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+    //                 target="_blank"
+    //                 rel="noopener noreferrer"
+    //             >
+    //                 Searchmind 2025
+    //             </a>
+    //         </footer>
+    //     </div>
+    // );
 }
